d56726701cc564f86dc1000db6084976a407cced
kcheung3102 <kcheung3102@gmail.com>
2019-10-27 22:29:12 -0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int livesLost = 0;
+	 int totalLives = 5;
+	 String[] wordList = new String[]{"tree","rain","bear","encourage","promise","soup","chess", "insurance","pancakes","stream"};
+	 String word = wordList[(int)(Math.random() * wordList.length)];
+	 Scanner input = new Scanner(System.in);
+	 String wordToGuess;
+
+    }
+}
